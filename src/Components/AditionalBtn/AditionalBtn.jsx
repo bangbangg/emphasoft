3c8777210b6a4c6412da572d7e22931ae1a5722b
baiumm <gorkir14@yandex.ru>
2020-11-28 19:37:13 +0300

    little hotfix font-size
    Слегка отредактирован код и добавлен шрифт алертов для маленьких разрешений у мобильных телефонов

--- src/Components/AditionalBtn/AditionalBtn.jsx
+++ src/Components/AditionalBtn/AditionalBtn.jsx
@@ -13,54 +13,54 @@ export const AditionalBtn = () => {
   return (
     <div className="addButtons">
       <div className="buttonsRow">
-      <button
-        type="button"
-        className="btn btn-primary active mgn mw"
-        onClick={() => {
-          dispatch(sortUsers(sortedUsers.sort((a, b) => a.id > b.id ? 1 : -1)));
-          window.location.reload();
-        }}
-      >
-        соритровать по ID( min  max)
-      </button>
-      <button
-        type="button"
-        className="btn btn-primary active mgn mw"
-        onClick={() => {
-          dispatch(sortUsers(sortedUsers.sort((a, b) => a.id > b.id ? -1 : 1)));
-          window.location.reload();
-        }}
-      >
-        соритровать по ID (max  min)
-      </button>
-      </div>
-      <div className="inputRow">
-      <form className="form-inline my-2 my-lg-0">
-        <input
-          className="form-control mr-sm-2 mwForm"
-          type="search"
-          placeholder="Поиск"
-          aria-label="Search"
-          value={search}
-          onChange={(ev) => {
-            setSearch(ev.target.value);
-          }}
-        />
         <button
-          className="btn btn-outline-success my-2 my-sm-0 mwForm"
           type="button"
-          onClick={(ev) => {
-            ev.preventDefault();
-            dispatch(
-              sortUsers(users.filter((item) =>
-                (item.username.toLowerCase().includes(search.toLowerCase())))),
-            );
+          className="btn btn-primary active mgn mw"
+          onClick={() => {
+            dispatch(sortUsers(sortedUsers.sort((a, b) => a.id > b.id ? 1 : -1)));
             window.location.reload();
           }}
         >
-          Поиск
+          соритровать по ID( min  max)
         </button>
-      </form>
+        <button
+          type="button"
+          className="btn btn-primary active mgn mw"
+          onClick={() => {
+            dispatch(sortUsers(sortedUsers.sort((a, b) => a.id > b.id ? -1 : 1)));
+            window.location.reload();
+          }}
+        >
+          соритровать по ID (max  min)
+        </button>
+      </div>
+      <div className="inputRow">
+        <form className="form-inline my-2 my-lg-0">
+          <input
+            className="form-control mr-sm-2 mwForm"
+            type="search"
+            placeholder="Поиск"
+            aria-label="Search"
+            value={search}
+            onChange={(ev) => {
+              setSearch(ev.target.value);
+            }}
+          />
+          <button
+            className="btn btn-outline-success my-2 my-sm-0 mwForm"
+            type="button"
+            onClick={(ev) => {
+              ev.preventDefault();
+              dispatch(
+                sortUsers(users.filter((item) =>
+                  (item.username.toLowerCase().includes(search.toLowerCase())))),
+              );
+              window.location.reload();
+            }}
+          >
+            Поиск
+          </button>
+        </form>
       </div>
     </div>
   );
